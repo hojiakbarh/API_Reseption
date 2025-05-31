089526de2417320472d3cfe626954b2309831ee7
hojiakbarh <hoshimjonovhojiakbar008gmail.com>
2025-05-31 19:07:25 +0500

    Initial commit

--- apps/views.py
+++ apps/views.py
@@ -5,7 +5,6 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 from apps.models import User
 from apps.serializers import UserModelSerializers
 
